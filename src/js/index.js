923b20ad06f1513b50f949e266ae849a17fc5217
SPenguin <egroup.jerrylee@gmail.com>
2018-01-26 12:42:02 +0800

    fixed path error

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import {
 	AppContainer,
 } from 'react-hot-loader';
 
-import App from 'scenes';
+import App from './scenes';
 
 const render = (Component) => {
 	ReactDOM.render(
--- src/js/scenes/index.js
+++ src/js/scenes/index.js
@@ -4,7 +4,7 @@ import {
 	Route,
 } from 'react-router-dom';
 
-import Header from 'components/Header';
+import Header from '../components/Header';
 import Home from './Home';
 import Page1 from './Page1';
 import Page2 from './Page2';
