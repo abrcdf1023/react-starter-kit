e40a4fe8a5a560202dd242e51d6aa1e691bb426e
SPenguin <egroup.jerrylee@gmail.com>
2018-01-26 20:21:27 +0800

    update eslint config

--- src/js/scenes/index.js
+++ src/js/scenes/index.js
@@ -3,6 +3,7 @@ import {
 	BrowserRouter as Router,
 	Route,
 } from 'react-router-dom';
+/* eslint import/no-extraneous-dependencies: "off" */
 import { hot } from 'react-hot-loader';
 
 import Header from '../components/Header';
