0b44723b7e633fe3925e8ca625f4e92eed6df009
SP_Penguin <abrcdf1023@gmail.com>
2018-06-28 09:01:47 +0800

    add an error boundry

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ import { Route, Switch } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-import { store, history } from './redux/configureStore'
 import { hot } from 'react-hot-loader'
+import { store, history } from './redux/configureStore'
 
 import {
   Dashboard, Home, Login, SignUp,
 } from '@/loadables'
 
+import ErrorBoundry from './ErrorBoundry'
+
 export default hot(module)(() => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </ConnectedRouter>
-  </Provider>
+  <ErrorBoundry>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </ConnectedRouter>
+    </Provider>
+  </ErrorBoundry>
 ))
